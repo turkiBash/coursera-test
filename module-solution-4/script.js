fdbfa25a13711ca82f5be970a37fc0d1bbdba4a3
Turki <teko_911@hotmail.com>
2022-11-26 22:08:06 +0300

    initial5

--- module-solution-4/script.js
+++ module-solution-4/script.js
@@ -1,15 +1,15 @@
 (function () {
   var names = [
-    'Ahmed',
-    'Salem',
-    'Salman',
-    'Mona',
-    'Meme',
-    'Nana',
-    'Lolo',
-    'Mona',
-    'Saad',
-    'Moe',
+    'Yaakov',
+    'John',
+    'Jen',
+    'Jason',
+    'Paul',
+    'Frank',
+    'Larry',
+    'Paula',
+    'Laura',
+    'Jim',
   ];
   for (var i = 0; i < names.length; i++) {
     var firstLetter = names[i].charAt(0).toLowerCase();
